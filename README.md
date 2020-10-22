6c0f5aec7104859034bb8e996fbd916ee37c50d2
Rafael Franco <rafaelfr@outlook.com>
2020-10-22 19:25:42 -0300

    docs: add docs page (#9)

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 [![NPM](https://img.shields.io/npm/l/styled-content-loader)](LICENSE.md)
 [![semantic-release](https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg)](https://github.com/semantic-release/semantic-release)
 
-The simplest solution for content loader in React and styled-components.
+The simplest solution for content loading in React and styled-components.
 
 ## Getting Started
 
---
+++ docs/src/components/Example.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live'
+import styled, { css } from 'styled-components'
+import ContentLoader from 'styled-content-loader'
+import { darken } from 'polished'
+import dracula from 'prism-react-renderer/themes/dracula'
+
+const Container = styled.div(
+  ({
+    theme: {
+      colors: { dark },
+      space,
+    },
+  }) => css`
+    display: flex;
+    margin: ${space(4)} 0;
+
+    textarea {
+      background-color: ${darken(0.05, dark)} !important;
+      border-radius: ${space(1)};
+    }
+  `,
+)
+
+const Example = styled.div(
+  ({
+    theme: {
+      colors: { dark, light },
+      space,
+    },
+  }) => css`
+    background-color: ${light};
+    color: ${dark};
+    border-radius: ${space(1)};
+    margin: ${space(2)};
+    padding: ${space(2)} ${space(4)};
+    width: ${space(50)};
+  `,
+)
+
+export default ({ backgroundColor, foregroundColor, isLoading }) => {
+  const code = `
+<ContentLoader
+  backgroundColor={backgroundColor}
+  foregroundColor={foregroundColor}
+  isLoading={isLoading}
+>
+  <span>⌛</span>
+  <h1>styled-content-loader</h1>
+  <p>
+    The simplest solution for content loading in 
+    React and styled-components.
+  </p>
+</ContentLoader>
+`
+
+  return (
+    <Container>
+      <LiveProvider
+        code={code}
+        scope={{ ContentLoader, backgroundColor, foregroundColor, isLoading }}
+        theme={dracula}
+      >
+        <LiveEditor />
+        <Example>
+          <LivePreview />
+          <LiveError />
+        </Example>
+      </LiveProvider>
+    </Container>
+  )
+}
